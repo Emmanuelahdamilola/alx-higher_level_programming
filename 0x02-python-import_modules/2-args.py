da0f1e10a7c7751c673918cc2a3c419e4b0d2465
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-07-13 19:46:13 -0700

    program that prints the number of and the list of its arguments.

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+arg = sys.argv
+num_args = len(arg) - 1
+
+if (num_args > 1):
+    print("{} arguments:".format(num_args))
+    for index in range(1, num_args + 1):
+        print("{}: {}".format(index, arg[index]))
+
+elif (num_args == 0):
+    print("{} arguments.".format(num_args))
+
+else:
+    print("{} argument:".format(num_args))
+    print("{}: {}".format(num_args, arg[1]))
