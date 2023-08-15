f2674598dfd0af1f1146e0218cdfef216de8facf
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-08-14 18:29:17 -0700

    function that returns the list of available attributes and methods of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
-class MyClass:
-    """Defines the class"""
+"""defines the method"""
 
-    def __init__(self):
-        """initializing the class to itself"""
-
-    self.x = 1
-    self.y = 2
-
-    def my_method(self):
-        print("Hello World")
-
-
-obj = MyClass()
-
-print(dir(obj))
+def lookup(obj):
+    """returns list of the directory attributes"""
+    return(dir(obj))
