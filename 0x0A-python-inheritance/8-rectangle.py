ee264f5c88186cec417bef427aaebb14a8dd5bb3
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-08-14 21:51:40 -0700

    class Rectangle that inherits from BaseGeometry

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,24 +1,45 @@
 #!/usr/bin/python3
-"""Defines a base class"""
 
 
 class BaseGeometry:
+    """
+    Defines BaseGeometry class
+    """
     def area(self):
-        """Calculates the area (to be implemented by subclasses)."""
-        raise NotImplementedError("area() method not implemented")
+        """
+        Raises an Exception with the message 'area() is not implemented'
+        """
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates that a value is a positive integer."""
-        if not isinstance(value, int) or value <= 0:
-            raise ValueError(f"{name} must be a positive integer")
+        """
+        Validates that value is an integer greater than 0
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
+    """
+    Rectangle class that inherits from BaseGeometry
+    """
     def __init__(self, width, height):
-        """Initializes a Rectangle with given width and height."""
-        self.__width = 0
-        self.__height = 0
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """
+        Instantiating with width and height
+        """
         self.__width = width
         self.__height = height
+
+    def __str__(self):
+        """
+        Returns string representation of the rectangle
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        """
+        Returns the area of the rectangle
+        """
+        return self.__width * self.__height
