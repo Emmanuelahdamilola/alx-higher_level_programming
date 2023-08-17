239d0adbd5406d791cfa77b8666227214d707b2f
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-08-16 17:42:38 -0700

    A class rectangle inherit from the class Biogeometry

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,45 +1,19 @@
 #!/usr/bin/python3
-
-
-class BaseGeometry:
-    """
-    Defines BaseGeometry class
-    """
-    def area(self):
-        """
-        Raises an Exception with the message 'area() is not implemented'
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validates that value is an integer greater than 0
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    Rectangle class that inherits from BaseGeometry
-    """
+    """Represents a rectangle using BaseGeometry."""
+
     def __init__(self, width, height):
+        """Intializing a new Rectangle.
+
+        Args:
+            width: The width of the new Rectangle.
+            height: The height of the new Rectangle.
         """
-        Instantiating with width and height
-        """
+        self.integer_validator("width", width)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
-
-    def __str__(self):
-        """
-        Returns string representation of the rectangle
-        """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-    def area(self):
-        """
-        Returns the area of the rectangle
-        """
-        return self.__width * self.__height
