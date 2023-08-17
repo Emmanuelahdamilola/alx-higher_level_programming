522a1eb685f86a718c0d1d4f8b2b050658794720
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-08-16 17:34:47 -0700

    An empty Biogeometry

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
+"""Defines empty class Biogeometry"""
+
 
 class BaseGeometry:
     """
     An empty class representing a base geometry.
     """
-    def __init__(self):
-        pass
 
-    def area(self):
-        raise NotImplementedError
-
-    def perimeter(self):
-        raise NotImplementedError
+    pass
