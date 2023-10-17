14c6cca06f8a0e17ea30cc6b17e01e39851c2b4c
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-10-16 17:52:56 -0700

    script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-//script that prints a square
+// script that prints a square
 
 if (isNaN(process.argv[2])) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
   const size = parseInt(process.argv[2]);
-  const squareX = "X".repeat(size);
+  const squareX = 'X'.repeat(size);
 
   for (let i = 0; i < size; i++) {
     console.log(squareX);
