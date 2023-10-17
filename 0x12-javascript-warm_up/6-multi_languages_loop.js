d4bbc3b3d140b0136a8ae555c1703d2fe9af6a40
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-10-16 17:51:07 -0700

    script that prints 3 lines by using an array of string and a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-//script that prints 3 lines
-//by using an array of string and a loop
+// script that prints 3 lines
+// by using an array of string and a loop
 
 const programmingLang = ['C is fun', 'Python is cool', 'javascript is amazing'];
 
-for (let i = 0; i < 3; i++){
-	console.log(programmingLang[i]);
+for (let i = 0; i < 3; i++) {
+  console.log(programmingLang[i]);
 }
