68e67c1187989c8940413e15ab69d13f0d9a663e
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-10-17 20:33:08 -0700

    Rectangle #1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// class Rectangle that defines a rectangle
+// The constructor takes 2 arguments w and
+// Initialize the instance attribute width with the value of w
+// Initialize the instance attribute height with the value of h
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
