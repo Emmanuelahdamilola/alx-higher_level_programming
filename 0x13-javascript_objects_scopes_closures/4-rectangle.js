d74ee6308505c7a0ab6ffb0e7e9fb5408152d914
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-10-17 20:35:11 -0700

    Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return {};
+    }
+
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let size = '';
+      for (let j = 0; j < this.width; j++) {
+        size = size + 'X';
+      }
+      console.log(size);
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
