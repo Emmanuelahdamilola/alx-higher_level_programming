d79ae3e168cba024663bc6cd127a9393e44ad186
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-10-31 08:23:20 -0700

    script that takes in the name of a state as an argument and lists all cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,38 +1,40 @@
 #!/usr/bin/python3
 """
-Script that takes in the name of a state as an argument and lists all cities of that state,
-using the database hbtn_0e_4_usa.
+Script that takes in the name of a state as an argument and lists all cities
 
 """
 
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == '__main':
+
+if __name__ == '__main__':
 
     # Establish a connection to the MySQL database
-    connection = MySQLdb.connect(
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
 
     )
 
     # Create a cursor to interact with the database
-    cursor = connection.cursor()
+    cur = db.cursor()
 
-    # Execute SQL query to retrieve cities of the specified state and sort by cities.id
-    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities " \
-                "JOIN states ON cities.state_id = states.id " \
-                "WHERE states.name = %s", [argv[4]])
+    # Execute SQL query to retrieve cities of the specified
+    # state and sort by cities.id
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                WHERE states.name = %s", [argv[4]])
 
     # Fetch and print the results
-    query_rows = cursor.fetchall()
-    print(", ".join([row[0] for row in query_rows]))
+    rows = cur.fetchall()
+    # Print each city name in a comma-separated format
+    for row in rows:
+        print(str(row[0]) + ", " + row[1])
 
     # Clean up resources
-    cursor.close()
-    connection.close()
-
+    cur.close()
+    db.close()
