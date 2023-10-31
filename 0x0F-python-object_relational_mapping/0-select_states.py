5eea19795be48cd5739ef7deadb823f53463375c
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-10-31 03:34:20 -0700

    fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,10 +6,10 @@ Script that lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 from sys import argv
 
-
 if __name__ == '__main__':
     # Establish a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
     # Create a cursor to interact with the database
     cur = db.cursor()
@@ -25,4 +25,3 @@ if __name__ == '__main__':
     # Clean up resources
     cur.close()
     db.close()
-
