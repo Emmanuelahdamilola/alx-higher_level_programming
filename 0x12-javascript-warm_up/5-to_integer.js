3f62e1cf2a0d3c236671f5bddd5bb4b549d06401
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-11-04 12:15:12 -0700

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
-// script that prints My number if the first
-// argument can be converted to an integer
 
-if (isNaN(process.argv[2])) {
-  console.log('Not a Number');
+// Check if there is a command-line argument and store it in 'inputArg'
+const inputArg = process.argv[2];
+
+// Check if 'inputArg' is a valid number
+if (inputArg && !isNaN(inputArg)) {
+  // Convert 'inputArg' to an integer and print it
+  console.log('My number: ' + parseInt(inputArg, 10));
 } else {
-  console.log('My Number: ' + parseInt(process.argv[2]));
+  // If 'inputArg' is not a number, print "Not a number"
+  console.log('Not a number');
 }
+
