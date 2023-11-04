66af82f69765b11d59e322f648b570f9352b33e9
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-11-04 12:40:55 -0700

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
 
-class Rectangle {
+// Define a class named 'Rectangle'
+module.exports = class Rectangle {
+  // Constructor function for the Rectangle class
   constructor (w, h) {
-    if ((w < 0) && (h < 0)) {
-      return {};
+    if (w <= 0 || h <= 0) {
+      // If the dimensions are invalid, create an instance of Rectangle
+      Object.create(Rectangle);
+    } else if (w && h) {
+      // If valid dimensions are provided, set the width and height properties
+      this.width = w;
+      this.height = h;
     }
-
-    this.width = w;
-    this.height = h;
   }
-}
+};
 
-module.exports = Rectangle;
