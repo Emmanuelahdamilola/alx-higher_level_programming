9e3c69a65006b50fff24635c4233688a169df54b
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-11-04 13:56:33 -0700

    Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,32 +1,40 @@
 #!/usr/bin/python3
 """
-Script that prints the State object with the name passed as argument
-from the database
-Using module SQLAlchemy
+This script lists all State objects from the database hbtn_0e_6_usa
+that have a name matching the specified name
+provided as a command-line argument.
 """
 
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import MySQLdb
 from sys import argv
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    # create an engine
+    # Create a SQLAlchemy engine for the MySQL database
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    # create a configured "Session" class
-    Session = sessionmaker(bind=engine)
-    # create a Session
-    session = Session()
+
+    # Create the tables in the database
     Base.metadata.create_all(engine)
 
-    # Query the database for the State object
-    # with the specified name
-    state = session.query(State).filter(State.name == state_name).first()
+    # Create a session to interact with the database
+    session = Session(engine)
 
-    if state:
-        print(state.id)
-    else:
+    # Initialize a flag to check if any matching State objects are found
+    flag = 0
+
+    # Query and print State objects with a name
+    # that matches the specified name
+    for state in session.query(State).filter_by(name=argv[4]).order_by(State.id).all():
+        print("{}".format(state.id))
+        flag = 1
+
+    # Check if any matching State objects were found
+    # and print "Not found" if none were found
+    if flag != 1:
         print("Not found")
 
+    # Close the session
     session.close()
