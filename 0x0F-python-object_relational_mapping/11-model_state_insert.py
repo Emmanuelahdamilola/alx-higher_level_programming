4259bed6050fdaf5f23cb8627ca0d70cf65ef14c
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-11-04 14:02:08 -0700

    Add a new state

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
 """
-Script that adds the State object “Louisiana” to the database
-Using module SQLAlchemy
+This script lists all State objects from the database hbtn_0e_6_usa and adds a new State object named "Louisiana" to the database.
 """
 
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import MySQLdb
 from sys import argv
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import update
 
 if __name__ == "__main__":
-    # create an engine
+    # Create a SQLAlchemy engine for the MySQL database
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
-        # Create a session to interact with the database
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    
+    # Create the tables in the database
+    Base.metadata.create_all(engine)
 
-    # Create a new State object for "Louisiana" 
-    # and add it to the database
+    # Create a session to interact with the database
+    session = Session(engine)
+
+    # Create a new State object with the name "Louisiana"
     new_state = State(name="Louisiana")
+    
+    # Add the new State object to the database and commit the transaction
     session.add(new_state)
     session.commit()
 
-    # Print the new states.id after creation
-    print(new_state.id)
+    # Query and print the State object with the name "Louisiana"
+    for state in session.query(State).filter_by(name="Louisiana").order_by(State.id).all():
+        print("{}".format(state.id))
 
+    # Close the session
     session.close()
