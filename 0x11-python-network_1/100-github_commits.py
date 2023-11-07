14b5311022754c2b9259e30d6f2c23ff7e46a968
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-11-07 09:04:16 -0800

    Time for an interview

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
-"""
-This script lists the 10 most recent commits (from the most recent to oldest)
-of the repository "rails" by the user "rails" on GitHub.
-"""
+"""Fetches GitHub user repository commits."""
+
 import requests
 from sys import argv
 
 if __name__ == "__main__":
-    """
-    Main function: Lists the 10 most recent commits (from the most recent to oldest)
-    of the repository "rails" by the user "rails" on GitHub.
-    """
-    repository_name = argv[1]
-    owner_username = argv[2]
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner_username, repository_name)
+    # Get the GitHub username and repository name from command line arguments
+    github_user = argv[1]
+    github_repo = argv[2]
 
-    # Send a GET request to the GitHub API to retrieve commit information.
-    response = requests.get(url)
-    commit_list = response.json()
+    # Construct the API URL for the GitHub repository's commits
+    api_url = f"https://api.github.com/repos/{github_user}/{github_repo}/commits"
+    
+    # Send a GET request to the GitHub API to retrieve commit data
+    response = requests.get(api_url)
 
-    try:
-        # Iterate through the first 10 commits and
-        # print the SHA and author's name.
-        for i in range(10):
-            commit = commit_list[i]
-            sha = commit.get('sha')
-            author_name = commit.get('commit').get('author').get('name')
-            print("{}: {}".format(sha, author_name))
-    except:
-        # Handle exceptions (e.g., fewer than 10 commits) and do nothing (pass).
-        pass
+    # Initialize a counter to limit the number of commits to display
+    commit_counter = 0
+
+    # Iterate through the commits, sorted by the author's date in descending order
+    for commit in sorted(response.json(), key=lambda c: c.get('commit').get('author').get('date'), reverse=True):
+        # Print the commit SHA and author's name
+        print(commit.get('sha') + ": ", end="")
+        print(commit.get('commit').get('author').get('name'))
+        
+        # Increment the commit counter
+        commit_counter += 1
+        
+        # Limit the number of displayed commits to 10
+        if commit_counter == 10:
+            break
 
