32659825ac301d2a34e531bc28ce2e544c990696
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-11-28 21:07:50 -0800

    script that prints all characters of a Star Wars movie

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+// Importing the 'request' module for making HTTP requests
+const request = require('request');
+
+// Retrieving the movie ID from command-line arguments
+const movieID = process.argv[2];
+
+// Constructing the URL for the Star Wars API based on the movie ID
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieID;
+
+// Making an asynchronous HTTP GET request to the specified URL
+request(url, async (error, response, body) => {
+  // Handling errors, if any
+  if (error) {
+    console.log(error); // Log the error to the console
+  }
+
+  // Parsing the JSON body of the response
+  const movieData = JSON.parse(body);
+
+  // Looping through each character URL in the movie data
+  for (const charURL of movieData.characters) {
+    // Using Promise to handle asynchronous character requests
+    await new Promise((resolve, reject) => {
+      // Making an HTTP GET request to the character URL
+      request(charURL, (err, r, body) => {
+        // Handling errors, if any
+        if (err) {
+          reject(err);
+        } else {
+          // Logging the name of the character to the console
+          console.log(JSON.parse(body).name);
+          resolve();
+        }
+      });
+    });
+  }
+});
